56e54d8f9356fb46d20197258e63634113e0206e
menelik <menelix.mesa@nauta.cu>
2020-03-17 14:06:20 -0400

    I working with json in nodejs

---
+++ json.js
@@ -0,0 +1,7 @@
+var json_var = {name : 'Menelik', lastname : 'Mesa Rojas', age : 36 };
+console.log(json_var);
+console.log("%j",json_var);
+console.log(`El nombre es : ${json_var.name} y tiene la edad de ${json_var.age} años`);
+
+console.log(__filename);
+console.log(__dirname);
