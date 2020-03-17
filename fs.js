d797fd6948b1928f66789a975800a5144d0752eb
menelik <menelix.mesa@nauta.cu>
2020-03-17 00:01:42 -0400

    Im used fs to read a file named texto.txt

--- fs.js
+++ fs.js
@@ -11,5 +11,15 @@ fs.writeFile('./texto.txt','My name is Menelik , Im ready to up!!',function(err)
     }
 })
 
-console.log('Last line');
+fs.readFile('./texto.txt',function(err,data){
+
+    if(err)
+    {
+        console.log('There is an error when it trying to read the file');
+    }
+    else
+    {
+        console.log(data.toString());
+    }
+})
 
